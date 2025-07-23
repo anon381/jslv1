938896ae6e3fcf53eacf178ecbf18d203b1db390
Yabets Maregn <yabetsanon@gmail.com>
2025-07-23 21:49:53 +0900

    chore: update Next.js config (no visual changes)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = {
   webpack: (config) => {
     config.plugins.push(new MiniCssExtractPlugin());
+    config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
 };
