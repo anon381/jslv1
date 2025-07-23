b5e374407ef663deb1ba3f43e6eb6b21d6ef19cf
Yabets Maregn <yabetsanon@gmail.com>
2025-07-23 21:50:09 +0900

    feat: add PinContainer demo component for testing and preview

---
+++ components/3d-pin-demo.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { PinContainer } from "@/components/ui/3d-pin";
+
+export default function AnimatedPinDemo() {
+  return (
+    <div className="h-[40rem] w-full flex items-center justify-center ">
+     
+    </div>
+  );
+}
