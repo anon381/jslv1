efcaa2613d17f3d9db6a06d13bbfb83b9a6629d4
Yabets Maregn <yabetsanon@gmail.com>
2025-07-28 14:37:22 +0900

    fix: allow img.youtube.com for next/image external domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
+  images: {
+    domains: ["img.youtube.com"],
+  },
   webpack: (config) => {
     config.plugins.push(new MiniCssExtractPlugin());
     config.resolve.alias['@'] = path.resolve(__dirname);
