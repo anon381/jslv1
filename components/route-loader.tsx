da16752fd7528a54d8ba7c404a1f25a2cceec8d5
Yabets Maregn <yabetsanon@gmail.com>
2025-08-24 22:21:11 +0900

    updated

--- components/route-loader.tsx
+++ components/route-loader.tsx
@@ -23,7 +23,6 @@ export default function RouteLoader({ label = 'Loading' }: RouteLoaderProps) {
         <div className="relative w-32 h-32 mb-6">
           <div className="absolute inset-0 rounded-full border border-[#B7C9E2]/15 animate-orbit-spin" />
           <div className="absolute inset-4 rounded-full border border-[#B7C9E2]/25 animate-orbit-spin-fast" />
-          <div className="absolute top-1/2 left-1/2 w-28 h-28 -translate-x-1/2 -translate-y-1/2 rounded-full border-4 border-[#B7C9E2]/30 border-t-[#FFFBEA] animate-spin" />
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[10px] tracking-[0.3em] text-[#FFFBEA] font-semibold">
             LOAD
           </div>
